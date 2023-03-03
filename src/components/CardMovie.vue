c2387078b94e146b65db75c7baf9fdf50742e522
egyfazri <egyfazri23@gmail.com>
2023-03-03 18:29:57 +0700

    feat(shared): create card movie

---
+++ src/components/CardMovie.vue
@@ -0,0 +1,81 @@
+<script>
+export default {
+  name: "card-movie",
+  props: ["movie"],
+};
+</script>
+
+<template>
+  <div class="main_container">
+    <img :src="movie.image" class="image" />
+    <div class="rating px-4 py-1">
+      <span class="font-bold text-white">{{ movie.rating }}</span>
+    </div>
+    <div class="flex justify-start mt-2">
+      <span class="text-white text-left font-semibold">{{ movie.name }}</span>
+    </div>
+    <div class="flex justify-start">
+      <span class="text-white text-left font-thin">{{ movie.year }}</span>
+    </div>
+
+    <div class="preview">
+      <div class="flex flex-col justify-center items-center h-full">
+        <div class="flex items-center">
+          <div class="h-100 flex items-center">
+            <img src="/logo/star.png" style="width: 25px" />
+            <span class="font-bold text-2xl text-white ml-2 mt-1">
+              {{ movie.rating }}
+            </span>
+          </div>
+        </div>
+        <div class="font-bold text-2xl text-white ml-2 my-14">
+          {{ movie.genre }}
+        </div>
+        <button
+          class="bg-rose-700 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full w-3/6"
+        >
+          VIEW
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.main_container {
+  position: relative;
+}
+.image {
+  opacity: 1;
+  display: block;
+  width: 100%;
+  height: auto;
+  transition: 0.5s ease;
+  backface-visibility: hidden;
+}
+.rating {
+  position: absolute;
+  top: 0;
+  right: 0;
+  background: rgb(0, 0, 0, 0.6);
+}
+.preview {
+  transition: 0.5s ease;
+  opacity: 0;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  text-align: center;
+  height: 100%;
+  width: 100%;
+}
+.main_container:hover .image {
+  opacity: 0.3;
+}
+
+.main_container:hover .preview {
+  opacity: 1;
+}
+</style>
